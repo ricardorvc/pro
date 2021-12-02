5d313b805eabbec5c23b6d63f05273a5c3d57d58
ricardorvc <91580675+ricardorvc@users.noreply.github.com>
2021-12-02 11:32:32 -0800

    Add files via upload

---
+++ proyecto_valenciana_tallerelectrico/crud_empleado/delete.php
@@ -0,0 +1,17 @@
+<?php
+
+include("conexion.php");
+$con=conectar();
+
+$id=$_GET['id'];
+
+$sql="DELETE FROM cliente  WHERE id='$id'";
+$query=mysqli_query($con,$sql);
+
+    if($query){
+        Header("Location: ../empleado.php");
+        
+    }else{
+        echo "hay un error";
+    }
+?>
---
+++ proyecto_valenciana_tallerelectrico/crud_repuestos/delete.php
@@ -0,0 +1,16 @@
+<?php
+
+include("conexion.php");
+$con=conectar();
+
+$id=$_GET['id'];
+
+$sql="DELETE FROM cliente  WHERE id='$id'";
+$query=mysqli_query($con,$sql);
+
+    if($query){
+        Header("Location: ../repuestos.php");
+    }else{
+        echo "hay un error";
+    }
+?>
